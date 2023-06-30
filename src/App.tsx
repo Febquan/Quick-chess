d2594172bb7ad13885f883af41ccf098bdb7323e
Đỗ Minh Quân <febquan1@gmail.com>
2023-07-01 03:35:35 +0700

    'fix-deploy2'

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import { Provider } from "react-redux";
 import store from "./store/store";
 
 import MainView from "./view/MainView";
-import Header from "./view/header/Header";
+import Header from "./view/my-header/Header";
 
 const App: React.FC = () => {
   const [theme, setTheme] = useState<MyTheme>(MyTheme.Dark);
